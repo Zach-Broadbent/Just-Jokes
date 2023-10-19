cbe997aa029908853a21d134d0f902ac49e11a8c
Zach-Broadbent <zbroadbent1983@xmail.dixietech.edu>
2023-10-19 14:37:26 -0600

    Initial Commit

---
+++ Joke App/Joke_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Joke_AppApp.swift
+//  Joke App
+//
+//  Created by Zach Broadbent on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Joke_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
